11d96460401925f14a82787c55c45c5fe9d263f4
shaun.moloi@kantar.com <Shaun.Moloi@kantar.com>
2018-11-08 11:45:16 +0200

    Need to reconfigure for multi images

--- classify_image.py
+++ classify_image.py
@@ -1,36 +1,3 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Simple image classification with Inception.
-
-Run image classification with Inception trained on ImageNet 2012 Challenge data
-set.
-
-This program creates a graph from a saved GraphDef protocol buffer,
-and runs inference on an input JPEG image. It outputs human readable
-strings of the top 5 predictions along with their probabilities.
-
-Change the --image_file argument to any jpg image to compute a
-classification of that image.
-
-Please see the tutorial and website for a detailed description of how
-to use this script to perform image recognition.
-
-https://tensorflow.org/tutorials/image_recognition/
-"""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -197,8 +164,11 @@ def main(_):
       print("image file: ",image)
       print("\n")
   else:    
+      
+      
+      #Multi paths
       image = (FLAGS.image_file if FLAGS.image_file else
-           os.path.join(FLAGS.model_dir, 'flower2.jpg'))
+           os.path.join(FLAGS.model_dir, 'T1.jpg'))
       print("\n")
       print("default image file chosen: ",image)
       print("\n")
